bb2959adf2d9b12db36cc8a448e4c089f033bd73
philliplagoc <philliplagoc@gmail.com>
2019-02-13 00:42:49 -0800

    working on filter

--- public/js/index.js
+++ public/js/index.js
@@ -6,32 +6,28 @@ $(document).ready(function() {
 
 function initializePage() {
 	console.log("JS connected!");
-	$(".dropdown-toggle").click(function(e) {
-		$(this).dropdown(); // TODO: Fix the bug where this is double-clicked
-	});
 	search();
+	dropdown();
 }
 
+// Handles search button
 function search() {
 	$("#searchbar-submit").on('click', function(e) {
 		e.preventDefault();
 		var text = $("#searchbar").val();
-		console.log(text);
-		// Get all of the div class exercise 
 		var exercises = $("div #exercise").get();
-		console.log(exercises);
-		filter(exercises, text);
+		searchFilter(exercises, text);
 	})
 }
 
 // Loop through all exercises, and hide those who don't match textToFind
-function filter(exercises, textToFind) {
+// From https://www.w3schools.com/howto/howto_js_filter_lists.asp
+function searchFilter(exercises, textToFind) {
 	var query = textToFind.toUpperCase();
 	for(var i = 0; i < exercises.length; i++) {
 		// TODO: Change to jQuery
 		var exercise = exercises[i].getElementsByTagName("a")[0];
 		var nameOfExercise = exercise.textContent || exercise.innerText;
-		// console.log(nameOfExercise);
 		if(nameOfExercise.toUpperCase().indexOf(query) > -1) {
 			exercises[i].style.display = "";
 		} else {
@@ -39,3 +35,20 @@ function filter(exercises, textToFind) {
 		}
 	}
 }
+
+// Handles dropdown filter
+function dropdown() {
+	$("#dropdownFilter").on('click', function(e) {
+		e.preventDefault();
+		$(this).dropdown();
+		// Get the buttons that can be pressed 
+		var filters = $("li.filter").get();
+		// TODO: jQuery this
+		// Alphabetize A-Z if pressing the first filter
+		var aToZBtn = filters[0].getElementsByTagName("button")[0];
+		console.log(aToZBtn);
+		// Alphabetize Z-A if pressing second filter
+	})
+}
+
+
