a2f01cdf45e09d217cc0aa628270b3187078b990
Patrick <pgarrett@ucsd.edu>
2019-03-04 17:17:41 -0800

    more analytics

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,8 +1,10 @@
 function infoTabClick_A() {
+	console.log("Info tab click A");
 	ga("send", "event", 'infoTab', 'click');
 }
 
 function infoTabClick_B() {
+	console.log("Info tab click B");
 	ga("send", "event", 'infoTab', 'click');
 }
 
