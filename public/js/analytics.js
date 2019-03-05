6bfe87e3a9d6f6cf59247779efcb1c35a2ca1f45
Patrick <pgarrett@ucsd.edu>
2019-03-04 17:00:20 -0800

    start of analytics

---
+++ public/js/analytics.js
@@ -0,0 +1,25 @@
+function infoTabClick_A() {
+	ga("send", "event", 'infoTab', 'click');
+}
+
+function infoTabClick_B() {
+	ga("send", "event", 'infoTab', 'click');
+}
+
+function recordTab_A() {
+	ga("send", "event", 'recordTab', 'click');
+}
+
+function recordTab_B() {
+	ga("send", "event", 'recordTab', 'click');
+}
+
+function progressTab_A() {
+	ga("send", "event", 'progressTab', 'click');
+}
+
+function progressTab_B() {
+	ga("send", "event", 'progressTab', 'click');
+}
+
+
